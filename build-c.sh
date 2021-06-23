507c0fa78dcd385721d66cc8b943f050a1e19d90
Alden Torres <aldenml@gmail.com>
2021-06-22 21:56:09 -0400

    add blst for BLS12-381 (#11)

--- build-c.sh
+++ build-c.sh
@@ -9,4 +9,5 @@ popd
 
 cmake -DCMAKE_BUILD_TYPE=Release -B build -G "Ninja" .
 cmake --build build --config Release --parallel 2 --target libsodium-external
+cmake --build build --config Release --parallel 2 --target blst-external
 cmake --build build --config Release --parallel 2
