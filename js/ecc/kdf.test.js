6dbd6cecf3e5ef06566f17cee1670b0f0aa21c45
Alden Torres <aldenml@gmail.com>
2021-06-23 21:59:38 -0400

    add HKDF-SHA-256 and HKDF-SHA-512

---
+++ js/ecc/kdf.test.js
@@ -0,0 +1,87 @@
+/*
+ * Copyright (c) 2021, Alden Torres
+ *
+ * Licensed under the terms of the MIT license.
+ * Copy of the license at https://opensource.org/licenses/MIT
+ */
+
+import libecc_module from "./libecc.js";
+import {buf2hex, hex2buf,} from "./util.js";
+import assert from "assert";
+
+describe("ecc_kdf_hkdf_sha256", () => {
+
+    // https://datatracker.ietf.org/doc/html/rfc5869#appendix-A
+
+    it("Test Case 1", async () => {
+        const libecc = await libecc_module();
+        const ikm = hex2buf("0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b");
+        const salt = hex2buf("000102030405060708090a0b0c");
+        const info = hex2buf("f0f1f2f3f4f5f6f7f8f9");
+        const outLen = 42;
+
+        let prk = new Uint8Array(32);
+        libecc.ecc_kdf_hkdf_sha256_extract(prk, salt, salt.length, ikm, ikm.length);
+        let okm = new Uint8Array(outLen);
+        libecc.ecc_kdf_hkdf_sha256_expand(okm, outLen, info, info.length, prk);
+
+        assert.strictEqual(buf2hex(prk), "077709362c2e32df0ddc3f0dc47bba6390b6c73bb50f9c3122ec844ad7c2b3e5");
+        assert.strictEqual(buf2hex(okm), "3cb25f25faacd57a90434f64d0362f2a2d2d0a90cf1a5a4c5db02d56ecc4c5bf34007208d5b887185865");
+    });
+
+    it("Test Case 3", async () => {
+        const libecc = await libecc_module();
+        const ikm = hex2buf("0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b0b");
+        const salt = hex2buf("");
+        const info = hex2buf("");
+        const outLen = 42;
+
+        let prk = new Uint8Array(32);
+        libecc.ecc_kdf_hkdf_sha256_extract(prk, salt, salt.length, ikm, ikm.length);
+        let okm = new Uint8Array(outLen);
+        libecc.ecc_kdf_hkdf_sha256_expand(okm, outLen, info, info.length, prk);
+
+        assert.strictEqual(buf2hex(prk), "19ef24a32c717b167f33a91d6f648bdf96596776afdb6377ac434c1c293ccb04");
+        assert.strictEqual(buf2hex(okm), "8da4e775a563c18f715f802a063c5a31b8a11f5c5ee1879ec3454e5f3c738d2d9d201395faa4b61a96c8");
+    });
+});
+
+describe("ecc_kdf_hkdf_sha512", () => {
+
+    // https://github.com/jedisct1/libsodium/blob/master/test/default/kdf_hkdf.c
+    // https://github.com/jedisct1/libsodium/blob/master/test/default/kdf_hkdf.exp
+
+    it("test 1", async () => {
+        const libecc = await libecc_module();
+
+        const master_key_len = 66;
+        let master_key = new Uint8Array(master_key_len);
+        for (let i = 0; i < master_key_len; i++) {
+            master_key[i] = i;
+        }
+
+        const salt_len = 77;
+        let salt = new Uint8Array(salt_len);
+        for (let i = 0; i < salt_len; i++) {
+            salt[i] = ~i;
+        }
+
+        const context_len = 88;
+        let context = new Uint8Array(context_len);
+        for (let i = 0; i < context_len; i++) {
+            context[i] = (i + 111);
+        }
+
+        let prk512 = new Uint8Array(64);
+        libecc.ecc_kdf_hkdf_sha512_extract(prk512, salt, salt.length, master_key, master_key.length);
+
+        assert.strictEqual(buf2hex(prk512), "2502bc897dc1b23f9f2d8c35d519c5280ea960bf9154ebb07d377a12a81a4794ea8bdc0cb6ec59ab3303f5cbd713027825715f8af2ac0203e560fd2e55f4ff2b");
+
+        // i = 10
+        const outLen = 10;
+        let okm = new Uint8Array(outLen);
+        context[0] = 10;
+        libecc.ecc_kdf_hkdf_sha512_expand(okm, outLen, context, context.length, prk512);
+        assert.strictEqual(buf2hex(okm), "046f63a1e2d606d7893e");
+    });
+});
---
+++ src/kdf.h
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2021, Alden Torres
+ *
+ * Licensed under the terms of the MIT license.
+ * Copy of the license at https://opensource.org/licenses/MIT
+ */
+
+#ifndef ECC_KDF_H
+#define ECC_KDF_H
+
+#include "export.h"
+
+#define ecc_kdf_hkdf_sha256_KEYSIZE 32U
+#define ecc_kdf_hkdf_sha256_SIZE_MIN 0U
+#define ecc_kdf_hkdf_sha256_SIZE_MAX (0xff * 32U)
+
+#define ecc_kdf_hkdf_sha512_KEYSIZE 64U
+#define ecc_kdf_hkdf_sha512_SIZE_MIN 0U
+#define ecc_kdf_hkdf_sha512_SIZE_MAX (0xff * 64U)
+
+ECC_EXPORT
+int ecc_kdf_hkdf_sha256_extract(BYTE *prk, const BYTE *salt, int salt_len, const BYTE *ikm, int ikm_len);
+
+ECC_EXPORT
+void ecc_kdf_hkdf_sha256_keygen(BYTE *prk);
+
+ECC_EXPORT
+int ecc_kdf_hkdf_sha256_expand(BYTE *out, int out_len, const BYTE *ctx, int ctx_len, const BYTE *prk);
+
+ECC_OPAQUE_EXPORT
+ECC_EXPORT
+int ecc_kdf_hkdf_sha512_extract(BYTE *prk, const BYTE *salt, int salt_len, const BYTE *ikm, int ikm_len);
+
+ECC_OPAQUE_EXPORT
+ECC_EXPORT
+void ecc_kdf_hkdf_sha512_keygen(BYTE *prk);
+
+ECC_OPAQUE_EXPORT
+ECC_EXPORT
+int ecc_kdf_hkdf_sha512_expand(BYTE *out, int out_len, const BYTE *ctx, int ctx_len, const BYTE *prk);
+
+#endif // ECC_KDF_H
