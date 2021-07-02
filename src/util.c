2d66e7aa692dbbb4db5e57898aaa0a9cda38e975
Alden Torres <aldenml@gmail.com>
2021-07-02 18:51:18 -0400

    refactor project layout (#20)

--- src/util.c
+++ src/util.c
@@ -90,3 +90,19 @@ void ecc_I2OSP(byte_t *out, int x, int xLen) {
         x = x >> 8;
     }
 }
+
+int ecc_is_zero(const BYTE *n, int len) {
+    return sodium_is_zero(n, len);
+}
+
+void ecc_increment(BYTE *n, int len) {
+    return sodium_increment(n, len);
+}
+
+void ecc_add(BYTE *a, const BYTE *b, int len) {
+    sodium_add(a, b, len);
+}
+
+void ecc_sub(BYTE *a, const BYTE *b, int len) {
+    sodium_sub(a, b, len);
+}
