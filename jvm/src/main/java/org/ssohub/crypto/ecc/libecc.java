9be0b29c7a1a22828ecf28da24b052e3bdd1d67d
Alden Torres <aldenml@gmail.com>
2021-07-10 00:46:57 -0400

    publish to maven central (#33)

--- jvm/src/main/java/org/ssohub/crypto/ecc/libecc.java
+++ jvm/src/main/java/org/ssohub/crypto/ecc/libecc.java
@@ -147,15 +147,16 @@ public final class libecc {
      * Takes two pointers to unsigned numbers encoded in little-endian
      * format and returns:
      * <p>
-     * -1 if a < b
+     * -1 if a &lt; b
      * 0 if a == b
-     * 1 if a > b
+     * 1 if a &gt; b
      * <p>
      * The comparison is done in constant time
      *
      * @param a   first unsigned integer argument
      * @param b   second unsigned integer argument
      * @param len the length of both `a` and `b`
+     * @return -1, 0 or 1
      */
     public static native int ecc_compare(byte[] a, byte[] b, int len);
 
@@ -339,7 +340,7 @@ public final class libecc {
     /**
      * Produces a uniformly random byte string using SHA-512.
      * <p>
-     * In order to make this method to use only the stack, len should be <= 256.
+     * In order to make this method to use only the stack, len should be &lt;= 256.
      * <p>
      * See https://datatracker.ietf.org/doc/html/draft-irtf-cfrg-hash-to-curve-11#section-5.4.1
      *
@@ -347,8 +348,8 @@ public final class libecc {
      * @param msg     a byte string
      * @param msg_len the length of `msg`
      * @param dst     a byte string of at most 255 bytes
-     * @param dst_len the length of `dst`, should be <= 256
-     * @param len     the length of the requested output in bytes, should be <= 256
+     * @param dst_len the length of `dst`, should be &lt;= 256
+     * @param len     the length of the requested output in bytes, should be &lt;= 256
      */
     public static native void ecc_h2c_expand_message_xmd_sha512(
         byte[] out,
@@ -442,7 +443,7 @@ public final class libecc {
 
     /**
      * In order to make this method not to use dynamic memory allocation, there is a
-     * limit of credential_identifier_len <= 200.
+     * limit of credential_identifier_len &lt;= 200.
      * <p>
      * See https://datatracker.ietf.org/doc/html/draft-irtf-cfrg-opaque-05#section-5.1.1.2
      *
