7c907143b9ea209b318fe298e3ae0335c011013b
Alden Torres <aldenml@gmail.com>
2021-07-29 00:32:54 -0400

    more

--- src/sign.h
+++ src/sign.h
@@ -10,20 +10,95 @@
 
 #include "export.h"
 
-// eth2
 // https://datatracker.ietf.org/doc/html/draft-irtf-cfrg-bls-signature-04
 // https://github.com/cfrg/draft-irtf-cfrg-bls-signature
+//
+// The variant implemented here is the minimal-pubkey-size: public keys are
+// points in G1, signatures are points in G2.
 
-#define ecc_sign_bls_eth2_PRIVATEKEYSIZE 32
+#define ecc_sign_bls12_381_PUBLICKEYSIZE 48
+
+#define ecc_sign_bls12_381_PRIVATEKEYSIZE 32
 
 /**
+ * Signature size.
+ */
+#define ecc_sign_bls12_381_SIGNATURESIZE 96
+
+/**
+ * Generates a secret key `sk` deterministically from a secret
+ * octet string `ikm`. The secret key is guaranteed to be nonzero.
  *
- * @param out_SK
- * @param IKM
- * @param IKM_len
+ * For security, `ikm` MUST be infeasible to guess, e.g., generated
+ * by a trusted source of randomness and be at least 32 bytes long.
+ *
+ * See https://datatracker.ietf.org/doc/html/draft-irtf-cfrg-bls-signature-04#section-2.3
+ *
+ * @param sk (output) a secret key
+ * @param ikm a secret octet string
+ * @param ikm_len the length of `ikm`
  */
 ECC_EXPORT
-void ecc_sign_bls_eth2_keygen(byte_t *sk, const byte_t *ikm, int ikm_len);
+void ecc_sign_bls12_381_KeyGen(byte_t *sk, const byte_t *ikm, int ikm_len);
 
+/**
+ * Takes a secret key `sk and outputs the corresponding public key `pk`.
+ *
+ * See https://datatracker.ietf.org/doc/html/draft-irtf-cfrg-bls-signature-04#section-2.4
+ *
+ * @param pk (output) a public key
+ * @param sk the secret key
+ */
+ECC_EXPORT
+void ecc_sign_bls12_381_SkToPk(byte_t *pk, const byte_t *sk);
+
+/**
+ * Ensures that a public key is valid.  In particular, it ensures
+ * that a public key represents a valid, non-identity point that
+ * is in the correct subgroup.
+ *
+ * See https://datatracker.ietf.org/doc/html/draft-irtf-cfrg-bls-signature-04#section-2.5
+ *
+ * @param pk a public key in the format output by SkToPk
+ * @return 0 for valid or -1 for invalid
+ */
+ECC_EXPORT
+int ecc_sign_bls12_381_KeyValidate(const byte_t *pk);
+
+/**
+ * Computes a signature from sk, a secret key, and a message msg
+ * and put the result in sig.
+ *
+ * See https://datatracker.ietf.org/doc/html/draft-irtf-cfrg-bls-signature-04#section-2.6
+ *
+ * @param sig (output) the signature
+ * @param msg input message
+ * @param msg_len the length of `msg`
+ * @param sk the secret key
+ */
+ECC_EXPORT
+void ecc_sign_bls12_381_CoreSign(
+    byte_t *sig,
+    const byte_t *msg, int msg_len,
+    const byte_t *sk
+);
+
+/**
+ * Checks that a signature is valid for the message under the public key pk.
+ *
+ * See https://datatracker.ietf.org/doc/html/draft-irtf-cfrg-bls-signature-04#section-2.7
+ *
+ * @param pk the public key
+ * @param msg input message
+ * @param msg_len the length of `msg`
+ * @param sig the signature
+ * @return 0 of valid, -1 if invalid
+ */
+ECC_EXPORT
+int ecc_sign_bls12_381_CoreVerify(
+    const byte_t *pk,
+    const byte_t *msg, int msg_len,
+    const byte_t *sig
+);
 
 #endif // ECC_SIGN_H
--- test/test_sign.c
+++ test/test_sign.c
@@ -14,19 +14,53 @@
 #include <cmocka.h>
 #include "ecc_log.h"
 
-static void ecc_sign_bls_eth2_keygen_test(void **state) {
+static void ecc_sign_bls12_381_keygen_test(void **state) {
     ECC_UNUSED(state);
 
-    byte_t seed[32] = {0};
-    //ecc_hex2bin(seed, "c55257c360c07c72029aebc1b53c05ed0362ada38ead3e3e9efa3708e53495531f09a6987599d18264c1e1c92f2cf141630c7a3c4ab7c81b2f001698e7463b04", 128);
-    byte_t sk[ecc_sign_bls_eth2_PRIVATEKEYSIZE];
-    ecc_sign_bls_eth2_keygen(sk, seed, sizeof seed);
+    byte_t seed[64] = {0};
+    ecc_hex2bin(seed,
+                "c55257c360c07c72029aebc1b53c05ed0362ada38ead3e3e9efa3708e53495531f09a6987599d18264c1e1c92f2cf141630c7a3c4ab7c81b2f001698e7463b04",
+                128);
+    byte_t sk[ecc_sign_bls12_381_PRIVATEKEYSIZE];
+    ecc_sign_bls12_381_KeyGen(sk, seed, sizeof seed);
     ecc_log("sk", sk, sizeof sk);
+}
+
+static void ecc_sign_bls12_381_test(void **state) {
+    ECC_UNUSED(state);
+
+    byte_t seed[64] = {0};
+    ecc_hex2bin(seed,
+                "c55257c360c07c72029aebc1b53c05ed0362ada38ead3e3e9efa3708e53495531f09a6987599d18264c1e1c92f2cf141630c7a3c4ab7c81b2f001698e7463b04",
+                128);
+
+    byte_t pk[ecc_sign_bls12_381_PUBLICKEYSIZE];
+    byte_t sk[ecc_sign_bls12_381_PRIVATEKEYSIZE];
+    ecc_sign_bls12_381_KeyGen(sk, seed, sizeof seed);
+    ecc_sign_bls12_381_SkToPk(pk, sk);
+
+    ecc_log("pk", pk, sizeof pk);
+    ecc_log("sk", sk, sizeof sk);
+
+    byte_t msg[5] = "hello";
+
+    byte_t sig[ecc_sign_bls12_381_SIGNATURESIZE];
+    ecc_sign_bls12_381_CoreSign(sig, msg, sizeof msg, sk);
+    ecc_log("sig", sig, sizeof sig);
+
+    int r = ecc_sign_bls12_381_CoreVerify(pk, msg, sizeof msg, sig);
+    assert_int_equal(r, 0);
+
+    // mess with the sig
+    sig[0] = 0;
+    r = ecc_sign_bls12_381_CoreVerify(pk, msg, sizeof msg, sig);
+    assert_int_equal(r, -1);
 }
 
 int main() {
     const struct CMUnitTest tests[] = {
-        cmocka_unit_test(ecc_sign_bls_eth2_keygen_test),
+        cmocka_unit_test(ecc_sign_bls12_381_keygen_test),
+        cmocka_unit_test(ecc_sign_bls12_381_test),
     };
 
     return cmocka_run_group_tests(tests, NULL, NULL);
