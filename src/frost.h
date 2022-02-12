1e6a5f32a22b2d2cffd0092ed184aced32ec4b91
Alden Torres <aldenml@gmail.com>
2022-02-11 19:25:15 -0500

    add frost derive_lagrange_coefficient (#76)

--- src/frost.h
+++ src/frost.h
@@ -13,6 +13,7 @@
 // https://github.com/cfrg/draft-irtf-cfrg-frost
 // https://cfrg.github.io/draft-irtf-cfrg-frost/draft-irtf-cfrg-frost.html
 // https://datatracker.ietf.org/doc/html/draft-irtf-cfrg-frost-01
+// https://en.wikipedia.org/wiki/Secure_multi-party_computation
 
 // const
 /**
@@ -39,9 +40,9 @@
  * Evaluate a polynomial f at a particular input x, i.e., y = f(x)
  * using Horner's method.
  *
- * @param value scalar result of the polynomial evaluated at input x, size:ecc_frost_ristretto255_sha512_SCALARSIZE
+ * @param[out] value scalar result of the polynomial evaluated at input x, size:ecc_frost_ristretto255_sha512_SCALARSIZE
  * @param x input at which to evaluate the polynomial, a scalar, size:ecc_frost_ristretto255_sha512_SCALARSIZE
- * @param coeffs the polynomial coefficients, a list of scalars: size:coeffs_len*ecc_frost_ristretto255_sha512_SCALARSIZE
+ * @param coeffs the polynomial coefficients, a list of scalars, size:coeffs_len*ecc_frost_ristretto255_sha512_SCALARSIZE
  * @param coeffs_len the number of coefficients in `coeffs`
  */
 ECC_EXPORT
@@ -49,6 +50,22 @@ void ecc_frost_ristretto255_sha512_polynomial_evaluate(
     byte_t *value,
     const byte_t *x,
     const byte_t *coeffs, int coeffs_len
+);
+
+/**
+ * Lagrange coefficients are used in FROST to evaluate a polynomial f at f(0),
+ * given a set of t other points, where f is represented as a set of coefficients.
+ *
+ * @param[out] L_i the i-th Lagrange coefficient, size:ecc_frost_ristretto255_sha512_SCALARSIZE
+ * @param x_i an x-coordinate contained in L, a scalar, size:ecc_frost_ristretto255_sha512_SCALARSIZE
+ * @param L the set of x-coordinates, each a scalar, size:L_len*ecc_frost_ristretto255_sha512_SCALARSIZE
+ * @param L_len the number of x-coordinates in `L`, it should be less than 256
+ */
+ECC_EXPORT
+void ecc_frost_ristretto255_sha512_derive_lagrange_coefficient(
+    byte_t *L_i,
+    const byte_t *x_i,
+    const byte_t *L, int L_len
 );
 
 #endif // ECC_FROST_H
--- test/test_frost.c
+++ test/test_frost.c
@@ -46,9 +46,42 @@ static void test_ecc_frost_ristretto255_sha512_polynomial_evaluate_small_numbers
     assert_memory_equal(v3, r3, ecc_frost_ristretto255_sha512_SCALARSIZE);
 }
 
+static void test_ecc_frost_ristretto255_sha512_derive_lagrange_coefficient_1(void **state) {
+    ECC_UNUSED(state);
+
+    byte_t x0[ecc_frost_ristretto255_sha512_SCALARSIZE] = {1, 0};
+    byte_t x1[ecc_frost_ristretto255_sha512_SCALARSIZE] = {2, 0};
+    byte_t x2[ecc_frost_ristretto255_sha512_SCALARSIZE] = {3, 0};
+    byte_t x3[ecc_frost_ristretto255_sha512_SCALARSIZE] = {4, 0};
+
+    byte_t L[4 * ecc_frost_ristretto255_sha512_SCALARSIZE];
+    ecc_concat4(
+        L,
+        x0, sizeof x0,
+        x1, sizeof x1,
+        x2, sizeof x2,
+        x3, sizeof x3
+    );
+
+    byte_t L_0[ecc_frost_ristretto255_sha512_SCALARSIZE];
+    ecc_frost_ristretto255_sha512_derive_lagrange_coefficient(L_0, x0, L, 4);
+    ecc_log("L_0", L_0, sizeof L_0);
+    char L_0_hex[2 * ecc_frost_ristretto255_sha512_SCALARSIZE + 1];
+    ecc_bin2hex(L_0_hex, L_0, sizeof L_0);
+    assert_string_equal(L_0_hex, "0400000000000000000000000000000000000000000000000000000000000000");
+
+    byte_t L_1[ecc_frost_ristretto255_sha512_SCALARSIZE];
+    ecc_frost_ristretto255_sha512_derive_lagrange_coefficient(L_1, x1, L, 4);
+    ecc_log("L_1", L_1, sizeof L_1);
+    char L_1_hex[2 * ecc_frost_ristretto255_sha512_SCALARSIZE + 1];
+    ecc_bin2hex(L_1_hex, L_1, sizeof L_1);
+    assert_string_equal(L_1_hex, "e7d3f55c1a631258d69cf7a2def9de1400000000000000000000000000000010");
+}
+
 int main() {
     const struct CMUnitTest tests[] = {
         cmocka_unit_test(test_ecc_frost_ristretto255_sha512_polynomial_evaluate_small_numbers),
+        cmocka_unit_test(test_ecc_frost_ristretto255_sha512_derive_lagrange_coefficient_1),
     };
 
     return cmocka_run_group_tests(tests, NULL, NULL);
