ffb083831be820d336fc52f0e8f5734c3d84fda6
Alden Torres <aldenml@gmail.com>
2022-12-05 12:58:55 -0500

    add unit test for sha512

--- test/test_hash.c
+++ test/test_hash.c
@@ -30,9 +30,33 @@ static void test_ecc_hash_sha256(void **state) {
     ecc_json_destroy(json);
 }
 
+static void test_ecc_hash_sha512(void **state) {
+    ECC_UNUSED(state);
+
+    ecc_json_t *json = ecc_json_load("../test/data/hash/sha512.json");
+
+    const int n = ecc_json_array_size(json, "vectors");
+
+    for (int i = 0; i < n; i++) {
+        ecc_json_t *item = ecc_json_array_item(json, "vectors", 0);
+        const char *input = ecc_json_string(item, "input");
+        const char *output = ecc_json_string(item, "output");
+
+        byte_t digest[ecc_hash_sha512_HASHSIZE];
+        ecc_hash_sha512(digest, (const byte_t *) input, (int) strlen(input));
+
+        char hex[2 * ecc_hash_sha512_HASHSIZE + 1];
+        ecc_bin2hex(hex, digest, sizeof digest);
+        assert_string_equal(hex, output);
+    }
+
+    ecc_json_destroy(json);
+}
+
 int main(void) {
     const struct CMUnitTest tests[] = {
         cmocka_unit_test(test_ecc_hash_sha256),
+        cmocka_unit_test(test_ecc_hash_sha512),
     };
 
     return cmocka_run_group_tests(tests, NULL, NULL);
