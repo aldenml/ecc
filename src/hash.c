77c748ab4f368e21fbf9e5c98b6041267e812c82
Alden Torres <aldenml@gmail.com>
2022-12-11 22:01:23 -0500

    add static asserts for hash constants

--- src/hash.c
+++ src/hash.c
@@ -6,6 +6,7 @@
  */
 
 #include "hash.h"
+#include <assert.h>
 
 #ifdef __GNUC__
 #pragma GCC diagnostic push
@@ -26,6 +27,9 @@
 #ifdef __GNUC__
 #pragma GCC diagnostic pop
 #endif
+
+static_assert(ecc_hash_sha256_HASHSIZE == crypto_hash_sha256_BYTES, "");
+static_assert(ecc_hash_sha512_HASHSIZE == crypto_hash_sha512_BYTES, "");
 
 void ecc_hash_sha256(byte_t *digest, const byte_t *input, const int input_len) {
     crypto_hash_sha256(digest, input, (unsigned long long) input_len);
