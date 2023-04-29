866984537ca940712d161cf5ba47e291f0259407
Alden Torres <aldenml@gmail.com>
2023-04-28 22:20:26 -0400

    more

--- build-ios.sh
+++ build-ios.sh
@@ -7,12 +7,11 @@ pushd deps/libsodium
 [ -d "build-aux" ] && make clean
 popd
 
-export ECC_CFLAGS="-arch arm64 -mmacosx-version-min=11"
+export ECC_CFLAGS="-arch arm64 -mios-version-min=9.0.0"
 
 cmake \
   -DCMAKE_BUILD_TYPE=Release \
   -DCMAKE_SYSTEM_NAME=iOS \
-  -DCMAKE_OSX_DEPLOYMENT_TARGET=11.0 \
   -B build -G Xcode .
 
 cmake --build build --config Release --parallel 2 \
