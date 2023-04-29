3befb17199c2583d9b31483c7b94fec4369de1fc
Alden Torres <aldenml@gmail.com>
2023-04-28 23:44:14 -0400

    more

--- build-ios.sh
+++ build-ios.sh
@@ -7,12 +7,15 @@ pushd deps/libsodium
 [ -d "build-aux" ] && make clean
 popd
 
-export ECC_CFLAGS="-arch arm64 -mios-version-min=9.0.0"
+export ECC_CFLAGS="-arch arm64 -mios-version-min=11.0"
 
+#  -DPLATFORM=SIMULATORARM64 \
 cmake \
   -DCMAKE_BUILD_TYPE=Release \
-  -DCMAKE_SYSTEM_NAME=iOS \
+  -DCMAKE_TOOLCHAIN_FILE=cmake/ios.toolchain.cmake \
+  -DENABLE_VISIBILITY=TRUE \
+  -DDEPLOYMENT_TARGET=11.0 \
+  -DPLATFORM=OS64 \
   -B build -G Xcode .
 
-cmake --build build --config Release --parallel 2 \
-  --target ecc_static -- -sdk iphonesimulator
+cmake --build build --config Release --parallel 2 --target ecc_static
